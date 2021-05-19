ce278faec9b9599742fba41a2c87fac6a750692c
Erwin_Ismael_Lopez_Jimenez <73193858+ErwinIsmael@users.noreply.github.com>
2021-05-19 09:03:59 -0500

    Update ejercicio2_examen.c

--- examen/ejercicio2_examen.c
+++ examen/ejercicio2_examen.c
@@ -8,15 +8,26 @@ for (int i=0;i<5;i++){
 	//printf("%c",arrayChar[i]);
 }
 char *punteroChar;
-punteroChar=&arrayChar[4];
+punteroChar=&arrayChar[0];
 char *punteroCharDos;
 punteroCharDos=&arrayChar[4];
 printf("\n%c\n",arrayChar[0]);
-printf("\n%c\n",*punteroChar[1]);
+printf("\n%c\n",*punteroChar);
 printf("\n%c\n",*punteroCharDos);
 printf("\n%c\n",arrayChar[4]);
-printf("\n%c\n",*punteroChar);
-
+printf("\n%c\n",*punteroChar *(arrayChar[0]+4));
+int cont;
+cont++;
+for (int i=0;i<5;i++){
+	printf("%c",*(punteroChar+cont));
+	cont++; 
+}
+printf("\n");
+fflush;
+for (int i=0;i<5;i++){
+	printf("%c",*(punteroChar+cont)); 
+	cont++;
+}
 
 
 
